48be9ab8cd0e14819311bc755dccf42468cd1598
woiiz <147359999+woiiz@users.noreply.github.com>
2025-03-23 22:44:41 +0800

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,46 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if user already exists
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return res.status(400).json({ message: 'User already exists' });
+  }
+
+  // Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new User({ username, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error registering user', error: err });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  // Find user by username
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  // Compare passwords
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: 'Invalid credentials' });
+  }
+
+  // Generate JWT token
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+
+  res.status(200).json({ token });
+};
